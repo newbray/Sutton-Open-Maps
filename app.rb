60327d377e888a57c11118ae7d159e6725aeac3b
Adrian Short <adrian.short@gmail.com>
2010-09-17 23:50:10 +0100

    Added KML download

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'dm-aggregates'
 require 'dm-validations'
 require 'dm-migrations'
 require 'dm-serializer'
+require 'builder'
 require 'haml'
 
 class Layer
@@ -45,7 +46,13 @@ end
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db.sqlite3")
 DataMapper.auto_upgrade!
 
+helpers do
 
+  # http://stackoverflow.com/questions/2950234/get-absolute-base-url-in-sinatra
+  def base_url
+    @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
+  end
+end
 
 get '/' do
   @layers = Layer.all(:order => [:title])
@@ -67,6 +74,58 @@ get '/maps/:slug.xml' do
   content_type :xml
   headers "Content-Disposition" => "attachment"
   "<?xml version='1.0'?>\n" + Layer.first(:slug => params[:slug]).places.to_xml
+end
+
+get '/maps/:slug.kml' do
+  @layer = Layer.first(:slug => params[:slug])
+  headers "Content-Disposition" => "inline",
+    "Content-Type" => "application/vnd.google-earth.kml+xml"
+
+  xml = Builder::XmlMarkup.new( :indent => 2 )
+  
+  # Output the XML prologue
+  xml.instruct!
+  
+  # Like all XML docs, our KML file has a single root element.
+  # In KML it's called <kml>
+  xml.kml :xmlns => "http://earth.google.com/kml/2.0" do
+    xml.Document do
+  
+      xml.name @layer.title
+  
+      # Common marker style
+      xml.Style( :id => "marker" ) do
+        xml.IconStyle do
+          xml.Icon do
+            xml.href base_url + "/icons/#{@layer.icon}"
+          end
+        end
+        xml.PolyStyle do
+          xml.color "ffff0000"
+        end
+      end
+  
+
+      for place in @layer.places
+  
+        xml.Placemark do
+        
+          # Give all placemarks a common style as specified earlier in the Style block
+          xml.styleUrl "#marker"
+          
+          xml.name place.title
+          
+          xml.description [ place.address, place.phone, place.description ].compact.join("<br /><br />")
+          
+          xml.Point do
+          
+            # KML coords are always longitude, latitude, altitude (in metres)
+            xml.coordinates "#{place.lng}, #{place.lat}, 0"
+          end
+        end
+      end
+    end  
+  end
 end
 
 get '/maps/:slug' do
