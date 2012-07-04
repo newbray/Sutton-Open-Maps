f98b4bd1e581591f99f44f42b7a59b6376efc9b7
Adrian Short <adrian.short@gmail.com>
2012-07-04 04:27:54 +0100

    Added Atom namespace

--- app.rb
+++ app.rb
@@ -147,7 +147,7 @@ get '/maps/:slug.rss' do
   # Output the XML prologue
   xml.instruct!
   
-  xml.rss :version => "2.0", :"xmlns:georss" => "http://www.georss.org/georss"  do
+  xml.rss :version => "2.0", :xmlns => "http://www.w3.org/2005/Atom", :"xmlns:georss" => "http://www.georss.org/georss"  do
     xml.channel do
   
       xml.title "#{@layer.title} in Sutton"
