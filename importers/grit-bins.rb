ceb55e251368e43ed5fa11b6682ad9d53bb1a3f7
Adrian Short <adrian.short@gmail.com>
2013-01-23 12:13:57 +0000

    Tweak for ruby 1.9; add refill requests to @SuttonGrit & report it online

--- importers/grit-bins.rb
+++ importers/grit-bins.rb
@@ -1,5 +1,7 @@
-require 'app'
-require 'fastercsv'
+require_relative '../app'
+require 'csv'
+require 'pp'
+require 'open-uri' # For URL encoding
 
 # Before running this script with a CSV file, prepare it so:
 #   - There is only a single line of column headings on the first line of the file
@@ -19,7 +21,7 @@ layer = Layer.first_or_create :title => "Grit Bins", :slug => 'grit-bins', :icon
 # layer.places.destroy
 columns = {}
 
-FasterCSV.foreach(ARGV[0]) do |row|
+CSV.foreach(ARGV[0]) do |row|
 
   count += 1
   
@@ -32,18 +34,20 @@ FasterCSV.foreach(ARGV[0]) do |row|
       columns[column] = position
       position += 1
     end
-    
-    puts columns.inspect
-
   else
     
     unless row[columns['lat']].nil?
 
       p row
+      
+      road_clean = row[columns['Road']].downcase.gsub(/\w+/) { |s| s.capitalize } # capitalize first letter of each word
+      report_it_url = "http://reportit.sutton.gov.uk/arsys/shared/ri_login.jsp"
+      twitter_url = URI::encode("https://twitter.com/intent/tweet?text=@SuttonGrit Please refill grit bin #{row[columns['GritBinID']]} at #{road_clean} #{row[columns['Location']]} /via http://suttonmaps.heroku.com/&related=adrianshort,Suttononline")
   
       place = Place.first_or_new(
-        'title' =>        row[columns['Road']],
-        'description' =>  "#{row[columns['Location']]}<br /><br />To get this bin refilled please phone <a href=\"tel:02087705070\">Sutton Council on <strong>020 8770 5070</strong></a> and quote <strong>grit bin number #{row[columns['GritBinID']]}</strong>.",
+#         http://bhanu.blogspot.co.uk/2007/03/capitalizing-first-letter-of-each-word.html
+        'title' =>        road_clean,
+        'description' =>  "#{row[columns['Location']]}<br /><br />Need a refill? <a href=\"#{report_it_url}\">Report it online</a> quoting <strong>grit bin number #{row[columns['GritBinID']]}</strong> or <a href=\"#{twitter_url}\">tweet to @SuttonGrit</a>.",
         'lat' =>          row[columns['lat']],
         'lng' =>          row[columns['lng']],
         'layer' =>        layer
